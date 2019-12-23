e94c39399f9fae0632cffd28eb518404c1e021e4
Callum Walley <callum.walley@nesi.org.nz>
2019-12-23 20:52:29 +1300

    .

--- licence_controller.py
+++ licence_controller.py
@@ -281,14 +281,14 @@ def print_panel():
 
     hour_index = dt.datetime.now().hour - 1
 
-    log.info("╔═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╗")
+    log.info("╔═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╦═════════════╗")
     log.info("║   Licence   ║    Server   ║    Status   ║    Total    ║ Average Use ║  Username   ║ In Use All  ║ In Use NeSI ║  Token Use  ║     Soak    ║")
     #log.info("╠═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╣")
     
     for value in licence_list.values():
         if value["enabled"]:
-            log.info("╠═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╣")
-            log.info("║" + fit_2_col(value["licence_name"]) + "║" + fit_2_col(value["server_name"]) + "║" + fit_2_col(value["server_status"]) + "║" + fit_2_col(value["real_total"]) + "║"  + fit_2_col(value["real_usage_all"]) + "║"  + fit_2_col(value["hourly_averages"][hour_index]) + "║             " + "║" + fit_2_col(value["real_usage_nesi"]) + "║" + fit_2_col(value["token_usage"]) + "║" + fit_2_col(value["token_soak"]) + "║" )
+            log.info("╠═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╬═════════════╣")
+            log.info("║" + fit_2_col(value["licence_name"]) + "║" + fit_2_col(value["server_name"]) + "║" + fit_2_col(value["server_status"]) + "║" + fit_2_col(value["real_total"]) + "║"  + fit_2_col(value["real_usage_all"]) + "║             " + "║"  + fit_2_col(value["hourly_averages"][hour_index]) + "║" + fit_2_col(value["real_usage_nesi"]) + "║" + fit_2_col(value["token_usage"]) + "║" + fit_2_col(value["token_soak"]) + "║" )
             if value["real_usage_nesi"]:
                 #log.info("╠═════════════╩═════════════╩═════════════╩═════════════╩═════════════╬═════════════╬═════════════╬═════════════╬═════════════╣")
                 #log.info("║                                                                     ║   User/s    ║ In Use NeSI ║  Token Use  ║   Socket/s  ║")
@@ -296,12 +296,12 @@ def print_panel():
 
                 for user, user_value in value["users_nesi"].items():  
      
-                    log.info("║             "*5 + "║" + fit_2_col(user) + "║" + fit_2_col(user_value["count"]) + "║" + fit_2_col(", ".join(user_value["sockets"]),27) + "║")
+                    log.info("║             "*6 + "║" + fit_2_col(user) + "║" + fit_2_col(user_value["count"]) + "║" + fit_2_col(", ".join(user_value["sockets"]),27) + "║")
 
                 #log.info("╠═════════════╦═════════════╦═════════════╦═════════════╦═════════════╬═════════════╬═════════════╬═════════════╬═════════════╣")
 
         
-    log.info("╚═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╝")
+    log.info("╚═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╩═════════════╝")
 
 def get_nesi_use():
 
