6450bd889f671eb1b4d14665fe32eb3e4ebdc779
Callum Walley <callum.walley@nesi.org.nz>
2019-12-23 13:13:31 +1300

    Fix user

--- licence_controller.py
+++ licence_controller.py
@@ -283,7 +283,7 @@ def print_panel():
         if value["enabled"]:
             log.info("║" + str(value["licence_name"]).center(13) + "║" + str(value["server_name"]).center(13) + "║" + str(value["server_status"]).center(13) + "║" + str(value["real_total"]).center(13) + "║"  + str(value["real_usage_all"]).center(13) + "║"  + str(value["hourly_averages"][hour_index]).center(13) + "║" + str(value["real_usage_nesi"]).center(13) + "║" + str(value["token_usage"]).center(13) + "║" + str(value["token_soak"]).center(13) + "║" )
             if value["real_usage_nesi"]:
-                for user, value in users_nesi:
+                for user, value in value["users_nesi"]:
                     log.info("                                             ║" + str(user).center(13) + "║" + str(value["count"]).center(13) + "║" + str(value["sockets"]).center(13) + "║" + "║" )
 
                 
